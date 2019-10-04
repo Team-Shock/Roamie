a1e118d4963ec36e1c10ce101da1ccc9c3cc51be
Siwin Lo <siwin.lo@gmail.com>
2019-10-04 15:17:39 -0400

    feat: added login page and alphabetized stylesheet

--- components/Trip.js
+++ components/Trip.js
@@ -1,41 +1,29 @@
-import React from 'react';
-import { Text, View, Image, StyleSheet, Button, ImageBackground} from 'react-native';
-import { TouchableHighlight } from 'react-native-gesture-handler';
-import { tsMethodSignature } from '@babel/types';
+import React from "react";
+import {
+  Text,
+  View,
+  Image,
+  StyleSheet,
+  Button,
+  ImageBackground
+} from "react-native";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { tsMethodSignature } from "@babel/types";
+import { styles } from "../Styles/styles";
 
 export function Trip(props) {
   return (
-    <View style={styles.postContainer}>
-      <ImageBackground source={{uri: props.tripInfo.imageUrl}} style={{width: '100%', height: '100%'}}>
-          <View >
-            <Text style={styles.eventTitle}>{props.tripInfo.name}</Text>
-          </View>	              
-      </ImageBackground>
+    <View style={styles.tripLogRow}>
+      <Image
+        source={{ uri: props.tripInfo.imageUrl }}
+        style={styles.listImage}
+      />
+      <View>
+        <Text style={styles.eventTitle}>{props.tripInfo.name}</Text>
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Hide from TripLog" />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  postContainer: {
-    flex: 1,
-    padding: 10,
-    borderBottomColor: '#dadada',
-    borderBottomWidth: 1,
-    height: 400
-  }, 
-  username: {
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-  dateUserContainer: {
-    marginLeft: 3
-  },
-  eventTitle: {
-    fontSize: 20
-  },
-  eventDetails: {
-    padding: 5,
-    fontSize: 10
-  }
-})
-
--- script/seed.js
+++ script/seed.js
@@ -1,60 +1,76 @@
-const {green, red} = require('chalk')
-const db = require('../server/db')
-const {Trip, User} = require('../server/db/models')
+const { green, red } = require("chalk");
+const db = require("../server/db");
+const { Trip, User } = require("../server/db/models");
 
 // here's some sample candies to get you started
 // feel free to edit these or add your own!
 const trips = [
   {
-    name: 'Barcelona Trip with Friends',
-    imageUrl:
-      'https://cdn.cnn.com/cnnnext/dam/assets/170706113411-spain.jpg',
-    sharingUrl: ''
+    name: "Barcelona Trip with Friends",
+    imageUrl: "https://cdn.cnn.com/cnnnext/dam/assets/170706113411-spain.jpg",
+    sharingUrl: ""
   },
   {
-    name: 'Costa Rica with Family',
+    name: "Costa Rica with Family",
     imageUrl:
-    'http://amp.entercostarica.com/images/auto-sized/new_ecr/680x340/pages/18-costa-rica-volcanoes.jpg',
-    sharingUrl: ''
+      "http://amp.entercostarica.com/images/auto-sized/new_ecr/680x340/pages/18-costa-rica-volcanoes.jpg",
+    sharingUrl: ""
+  },
+  {
+    name: "Naoshima Solo Trip",
+    imageUrl:
+      "https://photos.smugmug.com/Kyoto/Naoshima/i-pqm9V63/0/a0e01e81/L/shutterstock_568757074-L.jpg",
+    sharingUrl: ""
+  },
+  {
+    name: "Dumbo Date Night",
+    imageUrl:
+      "http://blog.newyorkpass.com/wp-content/uploads/2017/03/Brooklyn_-_The_Dumbo_View-wikipedia.jpg",
+    sharingUrl: ""
+  },
+  {
+    name: "Red Rocks Climbing Trip",
+    imageUrl:
+      "https://www.mountainphotography.com/images/xl/20180317-Red-Rock-Canyon-Sunrise.jpg",
+    sharingUrl: ""
   }
-]
+];
 
 async function seed() {
-  await db.sync({force: true})
-  console.log('db synced!')
+  await db.sync({ force: true });
+  console.log("db synced!");
 
   const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+    User.create({ email: "cody@email.com", password: "123" }),
+    User.create({ email: "murphy@email.com", password: "123" })
+  ]);
 
   await Promise.all(
     trips.map(trip => {
-      return Trip.create(trip)
+      return Trip.create(trip);
     })
-  )
+  );
 
+  console.log(`seeded ${users.length} users`);
+  console.log(`seeded ${trips.length} trips`);
 
-  console.log(`seeded ${users.length} users`)
-  console.log(`seeded ${trips.length} trips`)
-
-  console.log(`seeded successfully`)
+  console.log(`seeded successfully`);
 }
 
 // We've separated the `seed` function from the `runSeed` function.
 // This way we can isolate the error handling and exit trapping.
 // The `seed` function is concerned only with modifying the database.
 async function runSeed() {
-  console.log('seeding...')
+  console.log("seeding...");
   try {
-    await seed()
+    await seed();
   } catch (err) {
-    console.error(err)
-    process.exitCode = 1
+    console.error(err);
+    process.exitCode = 1;
   } finally {
-    console.log('closing db connection')
-    await db.close()
-    console.log('db connection closed')
+    console.log("closing db connection");
+    await db.close();
+    console.log("db connection closed");
   }
 }
 
@@ -62,8 +78,8 @@ async function runSeed() {
 // `Async` functions always return a promise, so we can use `catch` to handle
 // any errors that might occur inside of `seed`.
 if (module === require.main) {
-  runSeed()
+  runSeed();
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed
+module.exports = seed;
