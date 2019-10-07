cac80566e329a806799b423960d6d0555167eea6
Siwin Lo <siwin.lo@gmail.com>
2019-10-07 17:56:54 -0400

    feat(preferences redux): in progress, committing at EOD

--- App.js
+++ App.js
@@ -7,6 +7,8 @@ import { Ionicons } from "@expo/vector-icons";
 import AppNavigator from "./navigation/AppNavigator";
 import { PostgressWrapper } from "./postgres/postgres";
 import { styles } from "./Styles/styles";
+import { Provider } from "react-redux";
+import store from "./store/index";
 
 export default function App(props) {
   const [isLoadingComplete, setLoadingComplete] = useState(false);
@@ -21,10 +23,12 @@ export default function App(props) {
     );
   } else {
     return (
-      <View style={styles.container}>
-        {Platform.OS === "ios" && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
+      <Provider store={store}>
+        <View style={styles.container}>
+          {Platform.OS === "ios" && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      </Provider>
     );
   }
 }
