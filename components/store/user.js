995546c94a590ef367a99253c18aa856c27de3c1
Ericka Sylvester <sylvester.ericka@gmail.com>
2019-10-07 14:22:04 -0400

    feat(auth) : Adding support for user sign up

---
+++ components/store/user.js
@@ -0,0 +1,74 @@
+import { PostgresWrapper } from '../../postgres/postgres'
+import history from '../history'
+
+/**
+ * ACTION TYPES
+ */
+const GET_USER = 'GET_USER'
+const REMOVE_USER = 'REMOVE_USER'
+
+/**
+ * INITIAL STATE
+ */
+const defaultUser = {}
+
+/**
+ * ACTION CREATORS
+ */
+const getUser = user => ({type: GET_USER, user})
+const removeUser = () => ({type: REMOVE_USER})
+
+/**
+ * THUNK CREATORS
+ */
+export const me = () => async dispatch => {
+  try {
+    const instance = await PostgresWrapper.getInstance();
+    const res = await instance.get('/auth/me')
+    dispatch(getUser(res.data || defaultUser))
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const auth = (email, password, method) => async dispatch => {
+  let res
+  try {
+    const instance = await PostgresWrapper.getInstance();
+    res = await instance.post(`/auth/${method}`, {email, password})
+  } catch (authError) {
+    return dispatch(getUser({error: authError}))
+  }
+
+  try {
+    dispatch(getUser(res.data))
+    history.push('/home')
+  } catch (dispatchOrHistoryErr) {
+    console.error(dispatchOrHistoryErr)
+  }
+}
+
+export const logout = () => async dispatch => {
+  try {
+    const instance = await PostgresWrapper.getInstance();
+    await instance.post('/auth/logout')
+    dispatch(removeUser())
+    history.push('/login')
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+/**
+ * REDUCER
+ */
+export default function(state = defaultUser, action) {
+  switch (action.type) {
+    case GET_USER:
+      return action.user
+    case REMOVE_USER:
+      return defaultUser
+    default:
+      return state
+  }
+}
