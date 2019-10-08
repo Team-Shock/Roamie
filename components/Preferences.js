b15971aee8726291724554031d304ae6046fd63b
Siwin Lo <41877484+siwinlo@users.noreply.github.com>
2019-10-08 16:22:32 -0400

    Revert "Preferences"

--- components/Preferences.js
+++ components/Preferences.js
@@ -1,49 +1,27 @@
 import React, { Component } from "react";
-import { Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
 import { styles } from "../Styles/styles";
 import BackgroundButton from "./BackgroundButton";
 import { connect } from "react-redux";
-import { getPreferences, setPreferences } from "../store/preferences";
 
 class Preferences extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      loading: true,
-      preferences: []
+      selected: []
     };
-    this.handlePress = this.handlePress.bind(this);
   }
   async componentDidMount() {
     await this.props.getPreferences();
-    await this.setState({
-      loading: false,
-      preferences: this.props.preferences
-    });
   }
 
-  async handlePress() {
-    this.props.setPreferences(this.state.preferences);
-  }
   render() {
     return (
-      <View>
-        <View style={styles.preferencesContainer}>
-          {this.props.preferences.length > 0 ? (
-            this.props.preferences.map(pref => (
-              <BackgroundButton key={pref.id} pref={pref} />
-            ))
-          ) : (
-            <Text>Loading...</Text>
-          )}
-        </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            style={styles.button}
-            title="Submit edits to preferences"
-            onPress={this.handlePress}
-          />
-        </View>
+      <View style={styles.preferencesContainer}>
+        {this.state.selected.map(pref => (
+          <BackgroundButton key={pref.id} preference={pref.preference} />
+        ))}
       </View>
     );
   }
@@ -54,8 +32,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  getPreferences: () => dispatch(getPreferences()),
-  setPreferences: preferences => dispatch(setPreferences(preferences))
+  getPreferences: () => dispatch(getPreferences())
 });
 
 export default connect(
