46f40a63755fea855d3ab85b1c2b81ab84914142
Siwin Lo <siwin.lo@gmail.com>
2019-10-09 11:12:47 -0400

    feat(userpreferences): added UserPreferences join table boolean and modified associations

--- script/seed.js
+++ script/seed.js
@@ -1,34 +1,36 @@
-const {green, red} = require('chalk')
-const db = require('../server/db')
-const {Trip, User, Place} = require('../server/db/models')
+const { green, red } = require("chalk");
+const db = require("../server/db");
+const { Trip, User, Place } = require("../server/db/models");
 
 // here's some sample candies to get you started
 // feel free to edit these or add your own!
 const trips = [
   {
-    name: "Barcelona Trip with Friends",
+    name: "Barcelona Trip with ENEMIES",
     imageUrl: "https://cdn.cnn.com/cnnnext/dam/assets/170706113411-spain.jpg",
     sharingUrl: "",
-    startLocation: 'Carrer de Mallorca, 401, 08013 Barcelona, Spain',
-    endLocation: 'Parc de la Ciutadella, 08003 Barcelona, Spain',
-    sharingUrl: '',
-    status: 'complete'
+    startLocation: "Carrer de Mallorca, 401, 08013 Barcelona, Spain",
+    endLocation: "Parc de la Ciutadella, 08003 Barcelona, Spain",
+    sharingUrl: "",
+    status: "complete"
   },
   {
     name: "Costa Rica with Family",
     imageUrl:
       "http://amp.entercostarica.com/images/auto-sized/new_ecr/680x340/pages/18-costa-rica-volcanoes.jpg",
     sharingUrl: "",
-    startDate: '2019-07-15 04:05:02',
-    endDate: '2019-07-20 14:15:00',
-    startLocation: 'Calle Central Alfredo Volio, Merced, San José Province, San José, Costa Rica',
-    endLocation: 'Guanacaste Province, Playa Hermosa, Costa Rica',
-    sharingUrl: '',
-    status: 'complete'
+    startDate: "2019-07-15 04:05:02",
+    endDate: "2019-07-20 14:15:00",
+    startLocation:
+      "Calle Central Alfredo Volio, Merced, San José Province, San José, Costa Rica",
+    endLocation: "Guanacaste Province, Playa Hermosa, Costa Rica",
+    sharingUrl: "",
+    status: "complete"
   },
   {
     name: "Naoshima Solo Trip",
-    imageUrl: 'https://photos.smugmug.com/Kyoto/Naoshima/i-pqm9V63/0/a0e01e81/L/shutterstock_568757074-L.jpg',
+    imageUrl:
+      "https://photos.smugmug.com/Kyoto/Naoshima/i-pqm9V63/0/a0e01e81/L/shutterstock_568757074-L.jpg",
     sharingUrl: ""
   },
   {
@@ -43,30 +45,29 @@ const trips = [
       "https://www.mountainphotography.com/images/xl/20180317-Red-Rock-Canyon-Sunrise.jpg",
     sharingUrl: ""
   }
-]
+];
 
 const places = [
   {
-    name: 'Restaurante Silvestre',
-    imageUrl: '',
-    description: '',
-    date: '2019-07-16 12:00:00',
+    name: "Restaurante Silvestre",
+    imageUrl: "",
+    description: "",
+    date: "2019-07-16 12:00:00",
     locationAddress:
-    'Ave. 11 Calle 3A - 955, Barrio Amón, Amón, San José Province, San José, 10101, Costa Rica',
-    locationLat: '9.938935',
-    locationLong: '-84.076361',
-    visibility: 'true'
+      "Ave. 11 Calle 3A - 955, Barrio Amón, Amón, San José Province, San José, 10101, Costa Rica",
+    locationLat: "9.938935",
+    locationLong: "-84.076361",
+    visibility: "true"
   },
   {
-    name: 'Onna Cafe',
-    imageUrl: '',
-    description: '',
-    date: '2019-09-16 12:00:00',
-    locationAddress:
-    'Carrer de Santa Teresa, 1, 08012 Barcelona, Spain',
-    locationLat: '41.400135',
-    locationLong: '2.159935',
-    visibility: 'true'
+    name: "Onna Cafe",
+    imageUrl: "",
+    description: "",
+    date: "2019-09-16 12:00:00",
+    locationAddress: "Carrer de Santa Teresa, 1, 08012 Barcelona, Spain",
+    locationLat: "41.400135",
+    locationLong: "2.159935",
+    visibility: "true"
   }
 ];
 
@@ -75,21 +76,25 @@ async function seed() {
   console.log("db synced!");
 
   const users = await Promise.all([
-    User.create({name: 'Shiba Doo', email: 'shiba@email.com', password: '123'}),
-    User.create({name: 'Cody', email: 'cody@email.com', password: '123'})
-  ])
+    User.create({
+      name: "Shiba Doo",
+      email: "shiba@email.com",
+      password: "123"
+    }),
+    User.create({ name: "Cody", email: "cody@email.com", password: "123" })
+  ]);
 
   await Promise.all(
     trips.map(trip => {
       return Trip.create(trip);
     })
-  )
+  );
 
   await Promise.all(
     places.map(place => {
-      return Place.create(place)
+      return Place.create(place);
     })
-  )
+  );
 
   console.log(`seeded ${users.length} users`);
   console.log(`seeded ${trips.length} trips`);
