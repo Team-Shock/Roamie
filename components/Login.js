871df06d5b0816d08083b8a4e2ce876903ea6ef2
Siwin Lo <siwin.lo@gmail.com>
2019-10-09 11:30:58 -0400

    feat(login): added ericka's login, loginform, and user reducer

--- components/Login.js
+++ components/Login.js
@@ -3,13 +3,18 @@ import { StyleSheet, Text, View, Image, Button, Alert } from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome";
 import { styles } from "../Styles/styles";
 import LoginForm from "./LoginForm";
-import * as Facebook from 'expo-facebook';
-// import * as firebase from 'firebase'
+import * as Facebook from "expo-facebook";
+import { auth } from "../store/user-reducer";
+import { connect } from "react-redux";
 
-export default class Login extends Component {npm 
+class Login extends Component {
+  npm;
   constructor(props) {
     super(props);
-    this.state = { signedIn: false, name: "", photoUrl: "" };
+    this.state = { name: "", email: "" };
+  }
+  onLogIn() {
+    this.props.addOAuthUser(this.state.name, this.state.email);
   }
 
   signInWithFacebook = async () => {
@@ -19,21 +24,25 @@ export default class Login extends Component {npm
         token,
         expires,
         permissions,
-        declinedPermissions,
-      } = await Facebook.logInWithReadPermissionsAsync('755192041599866', {
-        permissions: ['public_profile', 'email'],
+        declinedPermissions
+      } = await Facebook.logInWithReadPermissionsAsync("755192041599866", {
+        permissions: ["public_profile", "email"]
       });
-      if (type === 'success') {
+      if (type === "success") {
         // Get the user's name using Facebook's Graph API
-        const response = await fetch(`https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,birthday,picture.type(large)`);
+        const response = await fetch(
+          `https://graph.facebook.com/me?access_token=${token}&fields=id,name,email,birthday,picture.type(large)`
+        );
         // console.log(await response.json())
-        Alert.alert('Logged in!', `Hi ${(await response.json()).name}!`);
+        const res = await response.json();
+        this.setState({ name: res.name, email: res.email });
+        this.onLogIn(name, email);
+        Alert.alert("Logged in!", `Welcome ${res.name}!`);
       } else {
         // type === 'cancel'
       }
     } catch ({ message }) {
       alert(`Facebook Login Error: ${message}`);
-
     }
   };
 
@@ -47,12 +56,12 @@ export default class Login extends Component {npm
         <Text style={styles.loginText}>Welcome to Roamie</Text>
         <Image source={logo} style={styles.logo} />
         <LoginForm />
-        <View style={styles.loginButtonContainer}>
+        {/* <View style={styles.loginButtonContainer}>
           <Icon.Button name="google" backgroundColor="#ffffff" color="#F277C6">
             Login with Google
           </Icon.Button>
-        </View>
-        <View style={styles.loginButtonContainer} >
+        </View> */}
+        <View style={styles.loginButtonContainer}>
           <Icon.Button
             name="facebook"
             backgroundColor="#ffffff"
@@ -61,8 +70,19 @@ export default class Login extends Component {npm
           >
             Login with Facebook
           </Icon.Button>
-        </View>      
+        </View>
       </View>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  addOAuthUser: (name, email) => dispatch(oauth(name, email))
+});
+
+const LogInOrSignUp = connect(
+  null,
+  mapDispatchToProps
+)(LoginForm);
+
+export default Login;
