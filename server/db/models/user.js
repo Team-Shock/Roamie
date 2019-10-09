6acaa20d519e37dc3da94003fef5302f8f471078
Helen <helenhisaac@gmail.com>
2019-10-09 14:26:10 -0400

    feat(model): cleaned up comments in user model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -15,16 +15,12 @@ const User = db.define('user', {
   },
   password: {
     type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('password');
     },
   },
   salt: {
     type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
     get() {
       return () => this.getDataValue('salt');
     },
@@ -36,16 +32,12 @@ const User = db.define('user', {
 
 module.exports = User;
 
-/**
- * instanceMethods
- */
+//instanceMethods
 User.prototype.correctPassword = function(candidatePwd) {
   return User.encryptPassword(candidatePwd, this.salt()) === this.password();
 };
 
-/**
- * classMethods
- */
+//classMethods
 User.generateSalt = function() {
   return crypto.randomBytes(16).toString('base64');
 };
@@ -58,9 +50,9 @@ User.encryptPassword = function(plainText, salt) {
     .digest('hex');
 };
 
-/**
- * hooks
- */
+// hook functions
+
+//hash password on change or set
 const setSaltAndPassword = user => {
   if (user.changed('password')) {
     user.salt = User.generateSalt();
@@ -68,6 +60,7 @@ const setSaltAndPassword = user => {
   }
 };
 
+//set initial preferences on new user instance
 const setDefaultPreferences = async user => {
   const preferences = await Preferences.findAll();
   preferences.map(async pref => {
@@ -75,6 +68,7 @@ const setDefaultPreferences = async user => {
   });
 };
 
+//hooks
 User.beforeCreate(setSaltAndPassword);
 User.afterCreate(setDefaultPreferences);
 User.beforeUpdate(setSaltAndPassword);
