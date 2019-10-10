e859fed2bfac3df1919407cf56333aab902df770
Siwin Lo <siwin.lo@gmail.com>
2019-10-10 11:40:04 -0400

    feat(password hiding): enabled textInput secure to hide important shiba data

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import { Text, TextInput, View, Button } from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import { styles } from '../Styles/styles';
-import { auth } from '../store/user-reducer';
-import { connect } from 'react-redux';
+import React from "react";
+import { Text, TextInput, View, Button } from "react-native";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { styles } from "../Styles/styles";
+import { auth } from "../store/user-reducer";
+import { connect } from "react-redux";
 
 class LoginForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '' };
+    this.state = { email: "", password: "" };
     this.onLogIn = this.onLogIn.bind(this);
     this.onSignUp = this.onSignUp.bind(this);
   }
 
   onLogIn() {
-    this.props.auth(this.state.email, this.state.password, 'login');
+    this.props.auth(this.state.email, this.state.password, "login");
   }
 
   onSignUp() {
-    this.props.auth(this.state.email, this.state.password, 'signup');
+    this.props.auth(this.state.email, this.state.password, "signup");
   }
   render() {
     return (
@@ -30,6 +30,7 @@ class LoginForm extends React.Component {
           value={this.state.email}
         />
         <TextInput
+          secureTextEntry={true}
           style={{ height: 40 }}
           placeholder="Your password"
           onChangeText={password => this.setState({ password })}
@@ -65,7 +66,7 @@ class LoginForm extends React.Component {
 }
 
 const mapDispatchToProps = dispatch => ({
-  auth: (email, password, method) => dispatch(auth(email, password, method)),
+  auth: (email, password, method) => dispatch(auth(email, password, method))
 });
 
 const LogInOrSignUp = connect(
--- store/preferences.js
+++ store/preferences.js
@@ -1,25 +1,8 @@
 import axios from "axios";
+import { defaultPreferences } from "../utils/defaultPreferences";
 
-//These preferences could be default preferences in the models, in which case the getPreferences thunk would GET them from the database and the setPreferences would PUT/POST them there as well
 const initialState = {
-  preferences: [
-    { id: 1, preference: "coffee", selected: true },
-    { id: 2, preference: "brunch", selected: true },
-    { id: 3, preference: "breakfast", selected: true },
-    { id: 4, preference: "dinner", selected: true },
-    { id: 5, preference: "$", selected: true },
-    { id: 6, preference: "$$", selected: true },
-    { id: 7, preference: "$$$", selected: true },
-    { id: 8, preference: "family friendly", selected: true },
-    { id: 9, preference: "dog friendly", selected: true },
-    { id: 10, preference: "romantic", selected: true },
-    { id: 11, preference: "laptop friendly", selected: true },
-    { id: 12, preference: "indie", selected: true },
-    { id: 13, preference: "local favorite", selected: true },
-    { id: 14, preference: "ice cream", selected: true },
-    { id: 15, preference: "scenic", selected: true },
-    { id: 16, preference: "outdoorsy", selected: true }
-  ]
+  preferences: defaultPreferences
 };
 
 // Action Types
@@ -37,7 +20,7 @@ const setThePreferences = preferences => ({
 });
 
 // Thunk Creators
-export const getPreferences = prefs => async dispatch => {
+export const getPreferences = () => async dispatch => {
   dispatch(gotPreferences(prefs));
 };
 
