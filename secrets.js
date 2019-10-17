6f2bb49c6b0eb9ef48ed0bb57a203a01c973f806
Ericka Sylvester <sylvester.ericka@gmail.com>
2019-10-17 16:32:12 -0400

    feat(heroku) - shhh

---
+++ secrets.js
@@ -0,0 +1,14 @@
+const yelpKey =
+  'wqK7j8NkZ3YCzw85lbV9ZgWS5OMEqxSdI12nVPUjFFGiTKBy_QQYYmw_xgDXGaKvP4dA7mCk0QEEaQrJlaTBlCLLscrkDtX2iFjwb0eAxuw59S9PEUqL4J_E_c2UXXYx';
+ 
+const googleKey = 'AIzaSyARDp1rHQ4Dpda3ohGsNEoLKDOGz3a4HEA';
+ 
+const facebookAppId = '755192041599866';
+ 
+const username = 'postgres'
+const host = 'roamie-db.ccsldvj96dsk.us-east-1.rds.amazonaws.com'
+const database = 'roamie'
+const pass = 'RoamieShock1907'
+ 
+ 
+module.exports = { yelpKey, pass, username, host, database, facebookAppId, googleKey };
